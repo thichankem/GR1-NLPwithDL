2f737d31a1e938433ed0f98725987565f1764569
thichankem <dophuhung.hn@gmail.com>
2026-06-11 00:07:03 +0700

    .

--- Assigment 2/a2/a2/parser_model.py
+++ Assigment 2/a2/a2/parser_model.py
@@ -72,10 +72,39 @@ class ParserModel(nn.Module):
         ###     Dropout: https://pytorch.org/docs/stable/nn.html#dropout-layers
         ### 
         ### See the PDF for hints.
+        self.embed_to_hidden_weight=nn.Parameter(
+            torch.empty(
+                self.n_features*self.embed_size,
+                self.hidden_size
+            )
+        )
 
+        
+        nn.init.xavier_uniform_(self.embed_to_hidden_weight)
 
+        self.embed_to_hidden_bias=nn.Parameter(
+            torch.empty(self.hidden_size)
+        )
 
+        nn.init.uniform_(self.embed_to_hidden_bias)
 
+        self.dropout=nn.Dropout(self.dropout_prob)
+
+        self.hidden_to_logits_weight=nn.Parameter(
+            torch.empty(
+                self.hidden_size,
+                self.n_classes
+            )
+        )
+        nn.init.xavier_uniform_(self.hidden_to_logits_weight)
+
+        
+        self.hidden_to_logits_bias=nn.Parameter(
+            torch.empty(
+                self.n_classes)
+        )
+        nn.init.uniform_(self.hidden_to_logits_bias)
+    
         ### END YOUR CODE
 
     def embedding_lookup(self, w):
@@ -106,7 +135,11 @@ class ParserModel(nn.Module):
         ###     Gather: https://pytorch.org/docs/stable/torch.html#torch.gather
         ###     View: https://pytorch.org/docs/stable/tensors.html#torch.Tensor.view
         ###     Flatten: https://pytorch.org/docs/stable/generated/torch.flatten.html
-        x = None
+
+        
+        x = self.embeddings[w]
+        x=x.view(x.size(0), -1)
+        
 
 
         ### END YOUR CODE
@@ -143,7 +176,15 @@ class ParserModel(nn.Module):
         ### Please see the following docs for support:
         ###     Matrix product: https://pytorch.org/docs/stable/torch.html#torch.matmul
         ###     ReLU: https://pytorch.org/docs/stable/nn.html?highlight=relu#torch.nn.functional.relu
-        logits = None
+        x=self.embedding_lookup(w)
+
+        h=torch.matmul(  x, self.embed_to_hidden_weight)+ self.embed_to_hidden_bias
+
+        h=F.relu(h)
+
+        h=self.dropout(h)
+
+        logits = torch.matmul(  h, self.hidden_to_logits_weight)+ self.hidden_to_logits_bias
 
         ### END YOUR CODE
         return logits
--- Assigment 2/a2/a2/parser_transitions.py
+++ Assigment 2/a2/a2/parser_transitions.py
@@ -32,7 +32,9 @@ class PartialParse(object):
         ### Note: The root token should be represented with the string "ROOT"
         ### Note: If you need to use the sentence object to initialize anything, make sure to not directly 
         ###       reference the sentence object.  That is, remember to NOT modify the sentence object. 
-
+        self.stack=["ROOT"]
+        self.buffer=list(sentence)
+        self.dependencies=[]
 
         ### END YOUR CODE
 
@@ -51,7 +53,20 @@ class PartialParse(object):
         ###         1. Shift
         ###         2. Left Arc
         ###         3. Right Arc
+        if transition=="S":
+            self.stack.append(self.buffer.pop(0))
 
+        elif transition=="LA":
+            self.dependencies.append(
+                (self.stack[-1], self.stack[-2])
+            )
+            self.stack.pop(-2)
+
+        elif transition=="RA":
+            self.dependencies.append(
+                (self.stack[-2], self.stack[-1])
+            )
+            self.stack.pop()
 
         ### END YOUR CODE
 
@@ -103,7 +118,27 @@ def minibatch_parse(sentences, model, batch_size):
     ###             to remove objects from the `unfinished_parses` list. This will free the underlying memory that
     ###             is being accessed by `partial_parses` and may cause your code to crash.
 
+    partial_parses=[
+        PartialParse(sentence)
+        for sentence in sentences
+    ]
 
+    unfinished_parses=partial_parses[:]
+
+    while len(unfinished_parses)>0:
+        mini_batch=unfinished_parses[:batch_size]
+        transitions=model.predict(mini_batch)
+
+        for pp, transition in zip(mini_batch, transitions):
+            pp.parse_step(transition)
+
+            if len(pp.buffer)==0 and len(pp.stack)==1:
+                unfinished_parses.remove(pp)
+
+    dependencies=[
+        pp.dependencies
+        for pp in partial_parses
+    ]
     ### END YOUR CODE
 
     return dependencies
